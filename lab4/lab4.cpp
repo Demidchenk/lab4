822e8e4ed45cf55f10a1e8c4fa5012402ccb1264
Alexey Demidchenko <alexey.demidchenko@gmail.com>
2021-05-30 21:34:09 +0300

    input fix 2

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -10,8 +10,9 @@ int main()
     //input >> header.id[0] >> header.id[1] >> header.filesize >> header.reserved[0] >> header.reserved[1] >> header.headersize >> header.infoSize >> header.width >> header.depth >> header.biPlanes >> header.bits >> header.biCompression >> header.biSizeImage >> header.biXPelsPerMeter >> header.biYPelsPerMeter >> header.biClrUsed >> header.biClrImportant;
     pixelData** pix = new pixelData * [header.width];
     for (int i = 0; i < header.width; i++)
-        for (int j = 0; j < header.depth; j++)
-            input >> pix[i][j].redComponent >> pix[i][j].greenComponent >> pix[i][j].blueComponent;
+        pix[i] = new pixelData[header.depth];
+
+
 
     int n;
     cin >> n;
