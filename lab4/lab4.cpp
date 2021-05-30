24d836868967afcd146a5e1d528bf705c2c883d5
Alexey Demidchenko <alexey.demidchenko@gmail.com>
2021-05-30 21:00:05 +0300

    input

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -1,6 +1,16 @@
-﻿#include <iostream>
+﻿#include "Header.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    ifstream input("test.bmp");
+    bmpHead header;
+    input >> header.id[0] >> header.id[1] >> header.filesize >> header.reserved[0] >> header.reserved[1] >> header.headersize >> header.infoSize >> header.width >> header.depth >> header.biPlanes >> header.bits >> header.biCompression >> header.biSizeImage >> header.biXPelsPerMeter >> header.biYPelsPerMeter >> header.biClrUsed >> header.biClrImportant;
+    pixelData** pix = new pixelData * [header.width];
+    for (int i = 0; i < header.width; i++)
+        for (int j = 0; j < header.depth; j++)
+            input >> pix[i][j].redComponent >> pix[i][j].greenComponent >> pix[i][j].blueComponent;
+
+
 }
