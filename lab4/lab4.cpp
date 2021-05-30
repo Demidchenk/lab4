18ee9528bc3ccd82e806e5243bf685c1fabd39ed
Alexey Demidchenko <alexey.demidchenko@gmail.com>
2021-05-30 21:31:55 +0300

    input fix 1

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -4,13 +4,22 @@ using namespace std;
 
 int main()
 {
-    ifstream input("test.bmp");
+    ifstream input("test.bmp", ios::binary);
     bmpHead header;
-    input >> header.id[0] >> header.id[1] >> header.filesize >> header.reserved[0] >> header.reserved[1] >> header.headersize >> header.infoSize >> header.width >> header.depth >> header.biPlanes >> header.bits >> header.biCompression >> header.biSizeImage >> header.biXPelsPerMeter >> header.biYPelsPerMeter >> header.biClrUsed >> header.biClrImportant;
+    input.read((char*)&header, sizeof(bmpHead));
+    //input >> header.id[0] >> header.id[1] >> header.filesize >> header.reserved[0] >> header.reserved[1] >> header.headersize >> header.infoSize >> header.width >> header.depth >> header.biPlanes >> header.bits >> header.biCompression >> header.biSizeImage >> header.biXPelsPerMeter >> header.biYPelsPerMeter >> header.biClrUsed >> header.biClrImportant;
     pixelData** pix = new pixelData * [header.width];
     for (int i = 0; i < header.width; i++)
         for (int j = 0; j < header.depth; j++)
             input >> pix[i][j].redComponent >> pix[i][j].greenComponent >> pix[i][j].blueComponent;
 
+    int n;
+    cin >> n;
+    ofstream output("output.bmp", ios::binary);
+    output << header.id[0] << header.id[1] << header.filesize << header.reserved[0] << header.reserved[1] << header.headersize << header.infoSize << header.width * n << header.depth * n << header.biPlanes << header.bits << header.biCompression << header.biSizeImage << header.biXPelsPerMeter << header.biYPelsPerMeter << header.biClrUsed << header.biClrImportant;
 
+    for (int i = 0; i < header.width * n; i++)
+        for (int j = 0; j < header.depth * n; j++)
+            output << pix[i / n][j / n].redComponent << pix[i / n][j / n].greenComponent << pix[i / n][j / n].blueComponent;
+    cout << "Done";
 }
